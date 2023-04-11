b095102f8415758238de976c45c8b2caf008bcb6
pkaracocuk <114610291+pkaracocuk@users.noreply.github.com>
2023-04-11 18:34:15 +0300

    readme düzenlendi

---
+++ main.js
@@ -0,0 +1,9 @@
+function acil() {
+    document.getElementById('aside').style.display = 'block';
+}
+
+
+function kapat() {
+    document.getElementById('aside').style.display = 'none';
+}
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>Tesla Website Clone</h1>
+<h2>Languages and Technologies</h2>
+<p>HTML5 CSS3 JavaScript</p>
+<img src="/images/ekran.gif">
